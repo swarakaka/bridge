50226df773a5c7fddceb48dee1b3fd5145de943c
swara <swara09@gmail.com>
2026-09-24 23:05:00 +0200

    fix(core): Update eslint ignores for coverage directory

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,7 @@ export default tseslint.config(
     ignores: [
       '**/node_modules/**',
       '**/dist/**',
+      '**/coverage/**',
       '**/vendor/**',
       'packages/protocol/src/generated/**',
       'playground/public/**',
